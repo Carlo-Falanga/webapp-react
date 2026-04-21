4234806eaf69e422521999144ad24b1a61043486
Carlo-Falanga <carlofalanga7@gmail.com>
2026-04-21 12:34:23 +0200

    complete milestone 2: add base layout and configure app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import DefaultLayout from "./layouts/DefaultLayout"
+import HomePage from "./pages/HomePage"
+import MoviePage from "./pages/MoviePage"
 
 function App() {
 
   return (
     <>
-      <h1>Welcome to the App</h1>
+      <BrowserRouter>
+        <Routes>
+            <Route element={<DefaultLayout />}> 
+              <Route index element={<HomePage />} />
+              <Route path="/movies/:id" element={<MoviePage />} />
+            </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/components/AppFooter.jsx
@@ -0,0 +1,8 @@
+export default function AppFooter() {
+
+    return(
+        <>
+        <h1>footer</h1>
+        </>
+    )
+}
