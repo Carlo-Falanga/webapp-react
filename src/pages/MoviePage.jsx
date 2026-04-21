684f3e21b2e270d6c9c4c01d6156376f363803af
Carlo-Falanga <carlofalanga7@gmail.com>
2026-04-21 15:13:22 +0200

    complete milestone 4: add api call for single movie in MoviePage

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,8 +1,45 @@
-export default function MoviePage(){
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
-    return(
-        <>
-            <h1>Movie Page</h1>
-        </>
-    )
+export default function MoviePage() {
+  const { movieId } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  const api_url =
+    import.meta.env.VITE_API_SERVER_ADDRESS + "/movies/" + movieId;
+
+  useEffect(() => {
+    // fetch the api using axios
+    axios
+      .get(api_url)
+      .then((response) => {
+        // do something with the response
+        setMovie(response.data);
+        console.log(response.data);
+        
+      })
+      .catch((error) => {
+        console.error("Error fetching movie data:", error);
+      });
+  }, [movieId]);
+
+  return (
+    <>
+      <h1>Movie Page: {movieId}</h1>
+      {movie ? (
+        <div>
+          <h2>{movie.title}</h2>
+          <p>{movie.description}</p>
+          {/* movie review text */}
+            {movie.reviews && movie.reviews.map((review) => (
+              <p key={review.id}>{review.text}</p>
+            ))}
+        </div>
+      ) : (
+        <p>Movie not found.</p>
+      )}
+    </>
+  );
 }
