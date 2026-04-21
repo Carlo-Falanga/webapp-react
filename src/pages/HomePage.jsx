3218d664de6aa3f50e5bbfe9be2916bf07454f4e
Carlo-Falanga <carlofalanga7@gmail.com>
2026-04-21 12:54:48 +0200

    complete milestone 3: configure backend cors and add api call for books list in home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,31 @@
-export default function HomePage(){
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-    return(
-        <>
-            <h1>Home Page</h1>
-        </>
-    )
+export default function HomePage() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const api_url = import.meta.env.VITE_API_SERVER_ADDRESS + "/movies/";
+
+    // fetch the api using axios
+    axios
+      .get(api_url)
+      .then((response) => {
+        setMovies(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>Home Page</h1>
+        <ul>
+            {movies.map((movie) => (
+                <li key={movie.id}>{movie.title}</li>
+            ))}
+        </ul>
+    </>
+  );
 }
