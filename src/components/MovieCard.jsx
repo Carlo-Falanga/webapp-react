a1372fe489deedd275d1e8be3c2da7fae54666f7
Carlo-Falanga <carlofalanga7@gmail.com>
2026-04-21 16:11:37 +0200

    improve MovieCard style and responsiveness (bonus)

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,11 +5,12 @@ export default function MovieCard({ movie }) {
     <div className="col">
       <div className="card h-100 shadow-sm">
         <img src={`http://${movie.image}`} className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">{movie.title}</h5>
+        <div className="card-body d-flex flex-column justify-content-between gap-2">
+          <h5 className="card-title fw-bold">{movie.title}</h5>
+          <div><span className="fw-semibold">Director:</span> {movie.director}</div>
           <p className="card-text">{movie.abstract}</p>
           <Link to={`/movies/${movie.id}`}>
-            <button className="btn btn-primary">View More</button>
+            <button className="btn btn-dark">View More</button>
           </Link>
         </div>
       </div>
