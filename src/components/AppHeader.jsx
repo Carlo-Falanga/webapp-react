c9db6ac977b9fe56a4353aa97212a5d90d3eae8c
Carlo-Falanga <carlofalanga7@gmail.com>
2026-04-21 15:28:20 +0200

    update AppHeader component with navbar layout (bonus)

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,8 +1,41 @@
-export default function AppHeader() {
+import { NavLink, Link } from "react-router";
 
-    return(
-        <>
-        <h1>header</h1>
-        </>
-    )
+
+export default function AppHeader() {
+  return (
+    <>
+      <nav className="navbar navbar-expand-md">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">
+            Movies
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse ms-auto" id="navbarNav">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link disabled" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link disabled" to="/movies">
+                  Admin
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
 }
